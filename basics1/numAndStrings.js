11a11178f8678b950ecb1bd8e79deb3459d71f90
kmehta88 <39373609+kmehta88@users.noreply.github.com>
2024-02-02 11:48:32 +0000

    string discussion

---
+++ basics1/numAndStrings.js
@@ -0,0 +1,25 @@
+// String looks like 'Hello'
+// String looks like "kapil"
+
+const firstname = 'kapil'
+const lastname = "mehta"
+
+console.log(firstname + ' ' + lastname);
+
+console.log(`my first name name is ${firstname} and last name is ${lastname}`); // template literal
+
+// const username = 'kapil.com' // short hand notation
+const username = new String('kapildotcom')  // Behind the scene
+console.log(username[0]);
+console.log(username.length);
+console.log(username.charAt(2));
+console.log(username.substring(2,4));
+
+let email = "  kapil@km.com  "
+console.log(email);
+console.log(email.trim());
+
+let emailTwo = "kmehta88@hotmail.com"
+console.log(emailTwo.replace('kmehta', 'devansh'));
+console.log(emailTwo.includes('kmehta'));
+console.log(emailTwo.toUpperCase());
