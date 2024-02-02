af985bd009ad9de61cdd2e6dccef913bdcbb46b9
kmehta88 <39373609+kmehta88@users.noreply.github.com>
2024-02-02 08:10:06 +0000

    variables

---
+++ basics1/console.js
@@ -0,0 +1,5 @@
+console.log("hello");
+console.error("hello");
+console.warn("hello");
+
+//try to run above in brwoser - console.
---
+++ basics1/variables.js
@@ -0,0 +1,10 @@
+// Variables - let
+//constants - const
+
+let userEmail = "test@test.com"
+const userPassword = "abc"
+
+//userPassword = "11222" // TypeError: Assignment to constant variable.
+
+console.log("userPassword"); // Prints String
+console.log(userPassword); // Prints variable 
